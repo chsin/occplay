7aadea23c20f889ea130059a1d9ea163ad9c46ef
Ruza Markov <ruza.markov@gmail.com>
2014-08-10 12:01:59 -0700

    step export added
    some of the cleanup
    
    some of the cleanup
    
    some of the cleanup
    
    some of the cleanup
    
    some of the cleanup
    
    some of the cleanup
    
    some of the cleanup
    
    some of the cleanup

--- rubiksCone.py
+++ rubiksCone.py
@@ -4,14 +4,25 @@ from OCC.gp import *
 from OCC.BRepPrimAPI import *
 from OCC.BRepAlgoAPI import *
 from OCC.StlAPI import *
+from OCC.STEPControl import *
 import numpy as np
 from scipy import linalg
 
-def toSTL(thingus, filename, ascii=False, deflection=0.001):
+def toSTL(shape, filename, ascii=False, deflection=0.001):
     stl_writer = StlAPI_Writer()
     stl_writer.SetASCIIMode(ascii)
     stl_writer.SetDeflection(deflection)
-    stl_writer.Write(thingus, filename)
+    stl_writer.Write(shape, filename)
+
+def toSTEP(shape, filename, verbose=False, tolerance=0.001):
+        stepWriter = STEPControl_Writer()
+        stepWriter.SetTolerance(tolerance)
+        if shape:
+            status = stepWriter.Transfer(shape, STEPControl_AsIs )
+        if status:
+            stepWriter.Write(filename)
+        if verbose:
+            stepWriter.PrintStatsTransfer()
 
 def make_point(coordinates):
     '''
@@ -52,8 +63,7 @@ def make_axis(apex, axis):
     direction = make_unit_vector(axis)
     return gp_Ax2(origin, direction)
 
-def sphere_from_center_and_radius( center,
-                                   radius ):
+def make_sphere(center, radius):
     '''
     Creates a sphere given center and radius.
     @param center: center of a sphere as a numpy array
@@ -62,13 +72,9 @@ def sphere_from_center_and_radius( center,
     @type radius: float
     '''
     point = make_point(center)
-    return BRepPrimAPI_MakeSphere( point, radius )
+    return BRepPrimAPI_MakeSphere(point, radius)
 
-def cone_from_point_height_directionvector_and_two_radii( apex,
-                                                          axis,
-                                                          height,
-                                                          radius1,
-                                                          radius2 ):
+def make_cone(apex, axis, height, radius1, radius2):
     """
     Creates a cone or a frustum.
     @param vector: vector at the beginning
@@ -83,319 +89,145 @@ def cone_from_point_height_directionvector_and_two_radii( apex,
     @type radius1: float
     """
     coneAxis = make_axis(apex, axis)
-    cone = BRepPrimAPI_MakeCone( coneAxis,
-                                 radius1,
-                                 radius2,
-                                 height )
+    cone = BRepPrimAPI_MakeCone(coneAxis, radius1, radius2, height)
     return cone
 
-def centered_cube_from_side( side ):
+def make_cube( side ):
     '''
     Creates a cube given its side.
     @param side: side of the cube
     @type side: float
     '''
     a = float(side / 2.0);
+    ones = np.ones(3, dtype=float)
+    return BRepPrimAPI_MakeBox(make_point(-a * ones), make_point( a * ones))
+
+def layer_shape(side, v, sign = 1):
+    a = float(side / 2.0) * sign
+    array1 = -a * np.ones(3, dtype=float)
     point1 = make_point(-a * np.ones(3, dtype=float))
-    point2 = make_point( a * np.ones(3, dtype=float))
-    return BRepPrimAPI_MakeBox(point1, point2)
-
-def top_layer( side ):
-    a = float(side / 2.0)
-    array1 = -a * np.ones(3, dtype=float)
-    point1 = make_point(array1)
-    array2 = np.array((-a/3.0 , a, a), dtype=float)
-    point2 = make_point(array2) 
-    return BRepPrimAPI_MakeBox(point1, point2)
-
-def bottom_layer( side ):
-    a = float(side / 2.0)
-    array1 = a * np.ones(3, dtype=float)
-    point1 = make_point(array1)
-    array2 = np.array((a/3.0 , -a, -a), dtype=float)
-    point2 = make_point(array2) 
-    return BRepPrimAPI_MakeBox(point1, point2)
-
-def left_layer( side ):
-    a = float(side / 2.0)
-    array1 = -a * np.ones(3, dtype=float)
-    point1 = make_point(array1)
-    array2 = np.array((a, a, -a / 3.0), dtype=float)
-    point2 = make_point(array2) 
-    return BRepPrimAPI_MakeBox(point1, point2)
-
-def right_layer( side ):
-    a = float(side / 2.0)
-    array1 = a * np.ones(3, dtype=float)
-    point1 = make_point(array1)
-    array2 = np.array((-a, -a, a / 3.0), dtype=float)
-    point2 = make_point(array2) 
-    return BRepPrimAPI_MakeBox(point1, point2)
-
-def front_layer( side ):
-    a = float(side / 2.0)
-    array1 = -a * np.ones(3, dtype=float)
-    point1 = make_point(array1)
-    array2 = np.array((a, -a / 3.0, a), dtype=float)
-    point2 = make_point(array2) 
-    return BRepPrimAPI_MakeBox(point1, point2)
-
-def back_layer( side ):
-    a = float(side / 2.0)
-    array1 = a * np.ones(3, dtype=float)
-    point1 = make_point(array1)
-    array2 = np.array((-a, a / 3.0, -a), dtype=float)
-    point2 = make_point(array2) 
-    return BRepPrimAPI_MakeBox(point1, point2)
+    array2 =  a *(np.ones(3, dtype=float) - 4./3 * v)
+    point2 = make_point(a *(np.ones(3, dtype=float) - 4./3 * v)) 
+    return BRepPrimAPI_MakeBox(point1, point2).Shape()
 
 def sphere_shape():
-    diameter = 2.0
+    diameter = 200.0
     origin = np.zeros(3, dtype=float)
-    sphere = sphere_from_center_and_radius( origin,
-                                            diameter / 2.0 )
+    sphere = make_sphere( origin, diameter / 2.0 )
     return sphere.Shape()
-    
-def bottom_cone():
-    # create cone
-    apex = np.array((-0.2, 0, 0), dtype=float)
-    axis = np.array((1, 0, 0), dtype=float)
-    height = 3.0
-    radius1 = 0.0
-    radius2 = 6.0
-
-    cone = cone_from_point_height_directionvector_and_two_radii( apex,
-                                                                 axis,
-                                                                 height,
-                                                                 radius1,
-                                                                 radius2 )
-    return cone.Shape()
    
-def top_cone():
-    # create cone
-    apex = np.array((0.2, 0, 0), dtype=float)
-    axis = np.array((-1, 0, 0), dtype=float)
-    height = 3.0
+def cone_shape(v):
+    # create cone with axis in v directon
+    apex = np.array(20 * v, dtype=float)
+    axis = np.array(-100 * v, dtype=float)
+    height = 300.0
     radius1 = 0.0
-    radius2 = 6.0
-
-    cone = cone_from_point_height_directionvector_and_two_radii( apex,
-                                                                 axis,
-                                                                 height,
-                                                                 radius1,
-                                                                 radius2 )
-    return cone.Shape()
-   
-def front_cone():
-    # create cone
-    apex = np.array((0, 0.2, 0), dtype=float)
-    axis = np.array((0, -1, 0), dtype=float)
-    height = 3.0
-    radius1 = 0.0
-    radius2 = 6.0
-
-    cone = cone_from_point_height_directionvector_and_two_radii( apex,
-                                                                 axis,
-                                                                 height,
-                                                                 radius1,
-                                                                 radius2 )
-    return cone.Shape()
-
-def back_cone():
-    # create cone
-    apex = np.array((0, -0.2, 0), dtype=float)
-    axis = np.array((0, 1, 0), dtype=float)
-    height = 3.0
-    radius1 = 0.0
-    radius2 = 6.0
-
-    cone = cone_from_point_height_directionvector_and_two_radii( apex,
-                                                                 axis,
-                                                                 height,
-                                                                 radius1,
-                                                                 radius2 )
-    return cone.Shape()
-
-   
-def left_cone():
-    # create cone
-    apex = np.array((0, 0, 0.2), dtype=float)
-    axis = np.array((0, 0, -1), dtype=float)
-    height = 3.0
-    radius1 = 0.0
-    radius2 = 6.0
-
-    cone = cone_from_point_height_directionvector_and_two_radii( apex,
-                                                                 axis,
-                                                                 height,
-                                                                 radius1,
-                                                                 radius2 )
-    return cone.Shape()
-
-def right_cone():
-    # create cone
-    apex = np.array((0, 0, -0.2), dtype=float)
-    axis = np.array((0, 0, 1), dtype=float)
-    height = 3.0
-    radius1 = 0.0
-    radius2 = 6.0
-
-    cone = cone_from_point_height_directionvector_and_two_radii( apex,
-                                                                 axis,
-                                                                 height,
-                                                                 radius1,
-                                                                 radius2 )
-    return cone.Shape()
+    radius2 = 600.0
+    return make_cone(apex, axis, height, radius1, radius2).Shape()
 
 def cube_shape():
-    side = 3.0
-    return centered_cube_from_side(side).Shape()
+    side = 300.0
+    return make_cube(side).Shape()
 
 def draw_sphere(event = None):
     display.DisplayColoredShape( sphere_shape() , 'RED' )
-
-def draw_cone_1(event=None):
-    display.DisplayColoredShape( bottom_cone() , 'GREEN' )
-
-def draw_cone_2(event=None):
-    display.DisplayColoredShape( top_cone() , 'GREEN' )
 
 def draw_cube(event=None):
     display.DisplayColoredShape( cube_shape() , 'WHITE' )
 
 def cut_bottom_cone():
-    sphereShape = sphere_shape()
-    coneShape = bottom_cone()
-    cone_minus_sphere = BRepAlgoAPI_Cut(coneShape, sphereShape).Shape()
+    coneShape = cone_shape(np.array((-1, 0, 0), dtype=np.float))
+    cone_minus_sphere = BRepAlgoAPI_Cut(coneShape, sphere_shape()).Shape()
     return BRepAlgoAPI_Common(cone_minus_sphere, cube_shape()).Shape()
 
 def cut_top_cone():
-    sphereShape = sphere_shape()
-    coneShape = top_cone()
-    cone_minus_sphere = BRepAlgoAPI_Cut(coneShape, sphereShape).Shape()
+    coneShape = cone_shape(np.array((1, 0, 0), dtype=np.float))
+    cone_minus_sphere = BRepAlgoAPI_Cut(coneShape, sphere_shape()).Shape()
     return BRepAlgoAPI_Common(cone_minus_sphere, cube_shape()).Shape()
 
-def bottom_cone_layer():
-    sphereShape = sphere_shape()
-    coneShape = bottom_cone()
-    layer = bottom_layer(3.0).Shape()
+def cone_layer_shape(direction, sign = 1.0):
+    coneShape = cone_shape(direction * sign)
+    layer = layer_shape(300.0, direction, sign)
     step1 =  BRepAlgoAPI_Common(coneShape, cube_shape()).Shape()
     step2 =  BRepAlgoAPI_Fuse(step1, layer).Shape()
-    return BRepAlgoAPI_Cut(step2, sphereShape).Shape()
+    return BRepAlgoAPI_Cut(step2, sphere_shape()).Shape()
 
-def top_cone_layer():
-    sphereShape = sphere_shape()
-    coneShape = top_cone()
-    layer = top_layer(3.0).Shape()
-    step1 =  BRepAlgoAPI_Common(coneShape, cube_shape()).Shape()
-    step2 =  BRepAlgoAPI_Fuse(step1, layer).Shape()
-    return BRepAlgoAPI_Cut(step2, sphereShape).Shape()
+def x_cone_layer(sign = 1.0):
+    direction = np.array((1, 0, 0), dtype=np.float)
+    return cone_layer_shape(direction, sign)
 
-def front_cone_layer():
-    sphereShape = sphere_shape()
-    coneShape = front_cone()
-    layer = front_layer(3.0).Shape()
-    step1 =  BRepAlgoAPI_Common(coneShape, cube_shape()).Shape()
-    step2 =  BRepAlgoAPI_Fuse(step1, layer).Shape()
-    return BRepAlgoAPI_Cut(step2, sphereShape).Shape()
+def y_cone_layer(sign = 1.0):
+    direction = np.array((0, 1, 0), dtype=np.float)
+    return cone_layer_shape(direction, sign)
 
-def right_cone_layer():
-    sphereShape = sphere_shape()
-    coneShape = right_cone()
-    layer = right_layer(3.0).Shape()
-    step1 =  BRepAlgoAPI_Common(coneShape, cube_shape()).Shape()
-    step2 =  BRepAlgoAPI_Fuse(step1, layer).Shape()
-    return BRepAlgoAPI_Cut(step2, sphereShape).Shape()
-
-def left_cone_layer():
-    sphereShape = sphere_shape()
-    coneShape = left_cone()
-    layer = left_layer(3.0).Shape()
-    step1 =  BRepAlgoAPI_Common(coneShape, cube_shape()).Shape()
-    step2 =  BRepAlgoAPI_Fuse(step1, layer).Shape()
-    return BRepAlgoAPI_Cut(step2, sphereShape).Shape()
-
-def back_cone_layer():
-    sphereShape = sphere_shape()
-    coneShape = back_cone()
-    layer = back_layer(3.0).Shape()
-    step1 = BRepAlgoAPI_Common(coneShape, cube_shape()).Shape()
-    step2 = BRepAlgoAPI_Fuse(step1, layer).Shape()
-    return BRepAlgoAPI_Cut(step2, sphereShape).Shape()
+def z_cone_layer(sign = 1.0):
+    direction = np.array((0, 0, 1), dtype=np.float)
+    return cone_layer_shape(direction, sign)
 
 def face_piece():
-    back = back_cone_layer()
-    left = left_cone_layer()
-    right = right_cone_layer()
-    top = top_cone_layer()
-    bottom = bottom_cone_layer()
-    step1 = BRepAlgoAPI_Cut(back, left).Shape()
-    step2 = BRepAlgoAPI_Cut(step1, right).Shape()
-    step3 = BRepAlgoAPI_Cut(step2, top).Shape()
-    return BRepAlgoAPI_Cut(step3, bottom).Shape()
+    step1 = BRepAlgoAPI_Cut(y_cone_layer(-1.0), z_cone_layer(-1.0)).Shape()
+    step2 = BRepAlgoAPI_Cut(step1, z_cone_layer(1.0)).Shape()
+    step3 = BRepAlgoAPI_Cut(step2, x_cone_layer(1.0)).Shape()
+    return BRepAlgoAPI_Cut(step3, x_cone_layer(-1.0)).Shape()
 
 def edge_piece():
-    back = back_cone_layer()
-    left = left_cone_layer()
-    top = top_cone_layer()
-    bottom = bottom_cone_layer()
-    step1 = BRepAlgoAPI_Common(back, left).Shape()
-    step2 = BRepAlgoAPI_Cut(step1, top).Shape()
-    return BRepAlgoAPI_Cut(step2, bottom).Shape()
+    step1 = BRepAlgoAPI_Common(y_cone_layer(-1.0), z_cone_layer(-1.0)).Shape()
+    step2 = BRepAlgoAPI_Cut(step1, x_cone_layer(1.0)).Shape()
+    return BRepAlgoAPI_Cut(step2, x_cone_layer(-1.0)).Shape()
 
 def corner_piece():
-    back = back_cone_layer()
-    left = left_cone_layer()
-    top = top_cone_layer()
-    step = BRepAlgoAPI_Common(back, left).Shape()
-    return BRepAlgoAPI_Common(step, top).Shape()
-
-def draw_cut_bottom_cone(event=None):
-    display.DisplayColoredShape( cut_bottom_cone() , 'BLACK' )
+    step = BRepAlgoAPI_Common(y_cone_layer(-1.0), z_cone_layer(-1.0)).Shape()
+    return BRepAlgoAPI_Common(step, x_cone_layer(1.0)).Shape()
 
 def draw_cut_top_cone(event=None):
-    display.DisplayColoredShape( cut_top_cone() , 'BLACK' )
+    display.DisplayColoredShape(cut_top_cone(), 'BLACK')
 
-def draw_bottom_layer(event=None):
-    display.DisplayColoredShape( bottom_cone_layer() , 'BLUE' )
+def draw_cut_bottom_cone(event=None):
+    display.DisplayColoredShape(cut_bottom_cone(), 'BLACK')
 
 def draw_top_layer(event=None):
-    display.DisplayColoredShape( top_cone_layer() , 'BLUE' )
+    display.DisplayColoredShape(x_cone_layer(1.0), 'BLUE')
+
+def draw_bottom_layer(event=None):
+    display.DisplayColoredShape(x_cone_layer(-1.0), 'BLUE')
 
 def draw_back_layer(event=None):
-    display.DisplayColoredShape( front_cone_layer() , 'BLUE' )
+    display.DisplayColoredShape( y_cone_layer(1.0), 'BLUE')
 
 def draw_front_layer(event=None):
-    display.DisplayColoredShape( back_cone_layer() , 'BLUE' )
-
-def draw_left_layer(event=None):
-    display.DisplayColoredShape( left_cone_layer() , 'BLUE' )
+    display.DisplayColoredShape(y_cone_layer(-1.0), 'BLUE')
 
 def draw_right_layer(event=None):
-    display.DisplayColoredShape( right_cone_layer() , 'BLUE' )
+    display.DisplayColoredShape(z_cone_layer(1.0), 'BLUE')
+
+def draw_left_layer(event=None):
+    display.DisplayColoredShape(z_cone_layer(-1.0), 'BLUE')
 
 def draw_face_piece(event=None):
-    display.DisplayColoredShape( face_piece() , 'CYAN' )
+    display.DisplayColoredShape(face_piece(), 'CYAN')
 
 def draw_edge_piece(event=None):
-    display.DisplayColoredShape( edge_piece() , 'GREEN' )
+    display.DisplayColoredShape(edge_piece(), 'GREEN')
 
 def draw_corner_piece(event=None):
-    display.DisplayColoredShape( corner_piece() , 'ORANGE' )
+    display.DisplayColoredShape(corner_piece(), 'BLACK')
 
 def erase_all(event=None):
     display.EraseAll()
 
 if __name__ == '__main__':
-    toSTL(corner_piece(), 'corner.stl')
-    toSTL(edge_piece(), 'edge.stl')
-    toSTL(face_piece(), 'face.stl')
+    # toSTEP(corner_piece(), 'corner.step')
+    # toSTEP(edge_piece(), 'edge.step')
+    # toSTEP(face_piece(), 'face.step')
+
+    # toSTL(corner_piece(), 'corner.stl')
+    # toSTL(edge_piece(), 'edge.stl')
+    # toSTL(face_piece(), 'face.stl')
 
     display, start_display, add_menu, add_function_to_menu = \
         init_display()
     add_menu('Draw')
     add_function_to_menu('Draw', draw_sphere)
-    add_function_to_menu('Draw', draw_cone_1)
-    add_function_to_menu('Draw', draw_cone_2)
     add_function_to_menu('Draw', draw_cube)
     add_function_to_menu('Draw', draw_cut_bottom_cone)
     add_function_to_menu('Draw', draw_cut_top_cone)
