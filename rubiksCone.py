c4eb08bef9627762b94539bb072a00a871841085
Ruza Markov <ruza.markov@gmail.com>
2014-08-12 13:44:12 -0700

    adding cylindrical shapes
    adding cylindrical shapes

--- rubiksCone.py
+++ rubiksCone.py
@@ -8,8 +8,15 @@ from OCC.STEPControl import *
 import numpy as np
 from scipy import linalg
 
-cubeSide = 300.0
-sphereDiameter = 200.0
+reScale = 3.93701
+cubeSide = 300.0 / reScale
+sphereRadius = 100.0 / reScale
+
+tanAngle = 2.0
+apexShift = sphereRadius * (np.sqrt(1.0 - (4.0/13.0)**2) / tanAngle - 
+                            4.0/13.0)
+cylinderHeight = 12.7 * 2 / reScale
+cylinderDiameter = 25.3 / reScale
 
 def toSTL(shape, filename, ascii=False, deflection=0.001):
     stl_writer = StlAPI_Writer()
@@ -77,12 +84,29 @@ def make_sphere(center, radius):
     point = make_point(center)
     return BRepPrimAPI_MakeSphere(point, radius)
 
+def make_cylinder(apex, axis, height, radius):
+    """
+    Creates a cylinder.
+    @param apex: start point of cylinder
+    @type apex: numpy.ndarray(3,)
+    @param axis: direction vector of the cylinder's main axis
+    @type directionvector: numpy.ndarray(3,)
+    @param height: height of cylinder
+    @type height: float
+    @param radius: radius of cylinder
+    @type radius: float
+    """
+    cylinderAxis = make_axis(apex, axis)
+    cylinder = BRepPrimAPI_MakeCylinder(cylinderAxis, radius, height)
+    return cylinder
+
+
 def make_cone(apex, axis, height, radius1, radius2):
     """
     Creates a cone or a frustum.
-    @param vector: vector at the beginning
-    @type vector: numpy.ndarray(3,)
-    @param directionvector: direction vector of the cone maina axis
+    param apex: start point of cone
+    @type apex: numpy.ndarray(3,)
+    @param axis: direction vector of the cone's main axis
     @type directionvector: numpy.ndarray(3,)
     @param height: cone height
     @type height: float
@@ -115,17 +139,21 @@ def layer_shape(side, v, sign = 1):
 
 def sphere_shape():
     origin = np.zeros(3, dtype=float)
-    sphere = make_sphere( origin, sphereDiameter / 2.0 )
+    sphere = make_sphere( origin, sphereRadius )
     return sphere.Shape()
+
+def cylinder_shape(axis):
+    apex = (sphereRadius - cylinderHeight) * axis / linalg.norm(axis)
+
+    radius = cylinderDiameter / 2.0
+    return make_cylinder(apex, axis, cylinderHeight * 2.0, radius).Shape()
    
-def cone_shape(v):
-    # create cone with axis in v directon
-    apex = np.array(20 * v, dtype=float)
-    axis = np.array(-100 * v, dtype=float)
-    height = 300.0
-    radius1 = 0.0
-    radius2 = 600.0
-    return make_cone(apex, axis, height, radius1, radius2).Shape()
+def cone_shape(axis):
+    # create cone with this axis
+    apex = apexShift * axis
+    height = cubeSide
+    radius = tanAngle * height
+    return make_cone(apex, -axis, height, 0.0, radius).Shape()
 
 def cube_shape():
     return make_cube(cubeSide).Shape()
@@ -166,7 +194,9 @@ def face_piece():
     step1 = BRepAlgoAPI_Cut(y_cone_layer(-1.0), z_cone_layer(-1.0)).Shape()
     step2 = BRepAlgoAPI_Cut(step1, z_cone_layer(1.0)).Shape()
     step3 = BRepAlgoAPI_Cut(step2, x_cone_layer(1.0)).Shape()
-    return BRepAlgoAPI_Cut(step3, x_cone_layer(-1.0)).Shape()
+    step4 = BRepAlgoAPI_Cut(step3, x_cone_layer(-1.0)).Shape()
+    v = np.array((0, 1, 0), dtype=np.float)
+    return BRepAlgoAPI_Cut(step4, cylinder_shape(v)).Shape()
 
 def edge_piece():
     step1 = BRepAlgoAPI_Common(y_cone_layer(-1.0), z_cone_layer(-1.0)).Shape()
@@ -182,6 +212,10 @@ def draw_cut_top_cone(event=None):
 
 def draw_cut_bottom_cone(event=None):
     display.DisplayColoredShape(cut_bottom_cone(), 'BLACK')
+
+def draw_cylinder(event=None):
+    v = np.array((0, 1, 0), dtype=np.float)
+    display.DisplayColoredShape(cylinder_shape(v), 'WHITE')
 
 def draw_white_layer(event=None):
     display.DisplayColoredShape(x_cone_layer(1.0), 'BLUE')
@@ -202,13 +236,13 @@ def draw_orange_layer(event=None):
     display.DisplayColoredShape(z_cone_layer(-1.0), 'BLUE')
 
 def draw_face_piece(event=None):
-    display.DisplayColoredShape(face_piece(), 'CYAN')
+    display.DisplayColoredShape(face_piece(), 'BLACK')
 
 def draw_edge_piece(event=None):
     display.DisplayColoredShape(edge_piece(), 'GREEN')
 
 def draw_corner_piece(event=None):
-    display.DisplayColoredShape(corner_piece(), 'BLACK')
+    display.DisplayColoredShape(corner_piece(), 'CYAN')
 
 def erase_all(event=None):
     display.EraseAll()
@@ -226,6 +260,7 @@ if __name__ == '__main__':
         init_display()
     add_menu('Draw')
     add_function_to_menu('Draw', draw_sphere)
+    add_function_to_menu('Draw', draw_cylinder)
     add_function_to_menu('Draw', draw_cube)
     add_function_to_menu('Draw', draw_cut_bottom_cone)
     add_function_to_menu('Draw', draw_cut_top_cone)
